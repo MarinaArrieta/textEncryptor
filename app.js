9d383195b3298960bb9c1b2320a47bc2902a1a08
MarinaArrieta <arrietamarina12@gmail.com>
2024-07-19 20:46:20 -0300

    Funcion encriptar y funcion desencriptar

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
+function encript(cadena){
+    let cadena2 = [];
+    let cadena3 = [];
+
+    for (i=0; i<cadena.length; i++){
+        cadena2 = cadena.replaceAll('a', 'ai').replaceAll('e', 'enter').replaceAll(/i/ig, 'imes').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+        cadena3 = cadena2.replaceAll('aimes', 'ai');
+    }
+
+    return cadena3;
+}
+console.log(encript('ue holaii a i aii ia'));
+
+function desencript(cadena){
+    let cadena2 = [];
+
+    for (i=0; i<cadena.length; i++){
+        cadena2 = cadena.replaceAll('ai', 'a').replaceAll('enter', 'e').replaceAll(/imes/ig, 'i').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+    }
+
+    return cadena2;
+}
+console.log(desencript('ufatenter hoberlaiimesimes ai imes aiimesimes imesai'));
