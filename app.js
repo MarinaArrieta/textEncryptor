00b7ee29ee1ad6da2850ff836954ec7f2c6dcc8b
MarinaArrieta <arrietamarina12@gmail.com>
2024-07-20 17:03:15 -0300

    funcion sin letras mayusculas, caracteres especiales o numeros

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-function encript(cadena){
-    let cadena2 = [];
-    let cadena3 = [];
+function messageAlert(caracter){
 
-    for (i=0; i<cadena.length; i++){
-        cadena2 = cadena.replaceAll('a', 'ai').replaceAll('e', 'enter').replaceAll(/i/ig, 'imes').replaceAll('o', 'ober').replaceAll('u', 'ufat');
-        cadena3 = cadena2.replaceAll('aimes', 'ai');
+    const mayuscula = /[A-Z]/;
+    const numero = /\d/i;
+    const espessialCaracter = /\W/i;
+    const tab = /\s/i;
+
+    for (i=0; i<caracter.length; i++){
+        if (mayuscula.test(caracter[i]) || numero.test(caracter[i]) || espessialCaracter.test(caracter[i]) && !tab.test(caracter[i])){
+            console.log('Solo letras minúsculas, sin acentos ni caracteres especiales');
+        } 
     }
-
-    return cadena3;
 }
-console.log(encript('ue holaii a i aii ia'));
 
-function desencript(cadena){
-    let cadena2 = [];
+messageAlert('yola');
 
-    for (i=0; i<cadena.length; i++){
-        cadena2 = cadena.replaceAll('ai', 'a').replaceAll('enter', 'e').replaceAll(/imes/ig, 'i').replaceAll('ober', 'o').replaceAll('ufat', 'u');
-    }
+// function encript(cadena){
+//     let cadena2 = [];
+//     let cadena3 = [];
 
-    return cadena2;
-}
-console.log(desencript('ufatenter hoberlaiimesimes ai imes aiimesimes imesai'));
+//     for (i=0; i<cadena.length; i++){
+//         cadena2 = cadena.replaceAll('a', 'ai').replaceAll('e', 'enter').replaceAll(/i/ig, 'imes').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+//         cadena3 = cadena2.replaceAll('aimes', 'ai');
+//     }
+
+//     return cadena3;
+// }
+// console.log(encript('ue holaii a i aii ia'));
+
+// function desencript(cadena){
+//     let cadena2 = [];
+
+//     for (i=0; i<cadena.length; i++){
+//         cadena2 = cadena.replaceAll('ai', 'a').replaceAll('enter', 'e').replaceAll(/imes/ig, 'i').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+//     }
+
+//     return cadena2;
+// }
+// console.log(desencript('ufatenter hoberlaiimesimes ai imes aiimesimes imesai'));
