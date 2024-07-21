0809fe07e84b36b33fcb7493f74b393c1d29d97c
MarinaArrieta <arrietamarina12@gmail.com>
2024-07-21 20:55:43 -0300

    Conexion entre el dom y el js con sus funciones

--- app.js
+++ app.js
@@ -1,39 +1,72 @@
+const inpuText = document.getElementById('input__text');
+const encrypt = document.getElementById('encrypt');
+const image = document.getElementById('image');
+const encrypText = document.getElementById('encrypt__text');
+const text = document.getElementById('text');
+const decrypt = document.getElementById('decrypt');
+
 function messageAlert(caracter){
 
-    const mayuscula = /[A-Z]/;
-    const numero = /\d/i;
+    caracter = inpuText.value;
+
+    const upperLetter = /[A-Z]/;
+    const number = /\d/i;
     const espessialCaracter = /\W/i;
     const tab = /\s/i;
 
+    if(!caracter.value){
+        image.style.display = 'block';
+        text.style.display = 'block';
+        encrypText.style.display = 'none';
+    }
+
     for (i=0; i<caracter.length; i++){
-        if (mayuscula.test(caracter[i]) || numero.test(caracter[i]) || espessialCaracter.test(caracter[i]) && !tab.test(caracter[i])){
-            console.log('Solo letras minúsculas, sin acentos ni caracteres especiales');
+        if (upperLetter.test(caracter[i]) || number.test(caracter[i]) || espessialCaracter.test(caracter[i]) && !tab.test(caracter[i])){
+            alert('Solo letras minúsculas, sin acentos ni caracteres especiales');
+            upperLetterNo = caracter.replace(upperLetter, '');
+            numberNo = upperLetterNo.replace(number, '');
+            espessialCaracterNo = numberNo.replace(espessialCaracter, '');
+            inpuText.value = espessialCaracterNo;
         } 
     }
 }
 
-messageAlert('yola');
+inpuText.addEventListener('input', ()=>{
+    messageAlert();
+})
 
-// function encript(cadena){
-//     let cadena2 = [];
-//     let cadena3 = [];
+function encript(cadena){
+    image.style.display = 'none';
+    text.style.display = 'none';
+    encrypText.style.display = 'block';
 
-//     for (i=0; i<cadena.length; i++){
-//         cadena2 = cadena.replaceAll('a', 'ai').replaceAll('e', 'enter').replaceAll(/i/ig, 'imes').replaceAll('o', 'ober').replaceAll('u', 'ufat');
-//         cadena3 = cadena2.replaceAll('aimes', 'ai');
-//     }
+    cadena = inpuText.value;
+    let cadena2 = [];
+    let cadena3 = [];
 
-//     return cadena3;
-// }
-// console.log(encript('ue holaii a i aii ia'));
+    for (i=0; i<cadena.length; i++){
+        cadena2 = cadena.replaceAll('a', 'ai').replaceAll('e', 'enter').replaceAll(/i/ig, 'imes').replaceAll('o', 'ober').replaceAll('u', 'ufat');
+        cadena3 = cadena2.replaceAll('aimes', 'ai');
+    }
 
-// function desencript(cadena){
-//     let cadena2 = [];
+    return encrypText.textContent = cadena3;
+}
 
-//     for (i=0; i<cadena.length; i++){
-//         cadena2 = cadena.replaceAll('ai', 'a').replaceAll('enter', 'e').replaceAll(/imes/ig, 'i').replaceAll('ober', 'o').replaceAll('ufat', 'u');
-//     }
+encrypt.addEventListener('click', ()=> {
+    encript();
+});
 
-//     return cadena2;
-// }
-// console.log(desencript('ufatenter hoberlaiimesimes ai imes aiimesimes imesai'));
+function decryptString(cadena){
+    cadena = encrypText.innerText;
+    let cadena2 = [];
+
+    for (i=0; i<cadena.length; i++){
+        cadena2 = cadena.replaceAll('ai', 'a').replaceAll('enter', 'e').replaceAll(/imes/ig, 'i').replaceAll('ober', 'o').replaceAll('ufat', 'u');
+    }
+
+    return encrypText.textContent = cadena2;
+}
+
+decrypt.addEventListener('click', ()=> {
+    decryptString();
+});
